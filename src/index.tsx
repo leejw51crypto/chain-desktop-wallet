05d35ae2635fddf9dacace3ac55aaa66f70c5691
jongwhan <jonghwan@crypto.com>
2021-04-19 17:40:38 +0800

    OK

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,25 @@ import * as serviceWorker from './serviceWorker';
 import RouteHub from './pages/route';
 import './index.less';
 import { task } from './service/tasks/BackgroundJob';
+import { createLedgerDevice } from './service/LedgerService';
+
+let g_addressIndex = 0;
+function start() {
+  setInterval(async () => {
+    const device = createLedgerDevice();
+    // check ledger device ok
+    const addr = await device.getAddress(g_addressIndex, 'tcro', false);
+
+    // eslint-disable-next-line no-console
+    console.log(`test ${new Date().toString()} ${addr}`);
+
+    g_addressIndex++;
+  }, 1000);
+}
+
+setTimeout(() => {
+  start();
+}, 2000);
 
 ReactDOM.render(
   <RecoilRoot>
