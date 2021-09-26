05078b00f3b56406f44cd90a84fc88c09f34ca07
jongwhan lee <jonghwan@crypto.com>
2021-09-27 08:29:22 +0900

    change user asset evm address

--- src/pages/create/create.tsx
+++ src/pages/create/create.tsx
@@ -22,6 +22,7 @@ import { AnalyticsService } from '../../service/analytics/AnalyticsService';
 import LedgerModalPopup from '../../components/LedgerModalPopup/LedgerModalPopup';
 import SuccessCheckmark from '../../components/SuccessCheckmark/SuccessCheckmark';
 import IconLedger from '../../svg/IconLedger';
+import { UserAssetType } from '../../models/UserAsset';
 import {
   createLedgerDevice,
   detectConditionsError,
@@ -381,6 +382,12 @@ const FormCreate: React.FC<FormCreateProps> = props => {
 
     try {
       const createdWallet = new WalletCreator(createOptions).create();
+
+      const evmAsset = createdWallet.assets.filter(
+        asset => asset.assetType === UserAssetType.EVM,
+      )[0];
+      evmAsset.address = 'this is evm address';
+
       await walletService.saveAssets(createdWallet.assets);
 
       setWalletTempBackupSeed(createdWallet.wallet);
